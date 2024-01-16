d863d37b044ffbf14e25335a52e87d6ec75e3b92
RaminFarazandeh <farazandeh.ramin@gmail.com>
2024-01-16 16:46:59 -0500

    created a new CustomerController

---
+++ src/main/java/com/ramin/cardealership/controller/CustomerController.java
@@ -0,0 +1,15 @@
+package com.ramin.cardealership.controller;
+
+import com.ramin.cardealership.request.CustomerRegistrationRequest;
+import com.ramin.cardealership.service.CustomerService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/customers")
+public class CustomerController {
+
+}
