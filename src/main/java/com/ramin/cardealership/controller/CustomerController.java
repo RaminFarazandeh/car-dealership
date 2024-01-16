7df6224e6457d951391f8e3ba70086354e2bbf52
RaminFarazandeh <farazandeh.ramin@gmail.com>
2024-01-16 17:10:51 -0500

    added new endpoints in the CustomerController

--- src/main/java/com/ramin/cardealership/controller/CustomerController.java
+++ src/main/java/com/ramin/cardealership/controller/CustomerController.java
@@ -2,7 +2,6 @@ package com.ramin.cardealership.controller;
 
 import com.ramin.cardealership.request.CustomerRegistrationRequest;
 import com.ramin.cardealership.service.CustomerService;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,5 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/v1/customers")
 public class CustomerController {
+    
+    private final CustomerService customerService;
 
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @PostMapping
+    public void registerCustomer(
+            @RequestBody CustomerRegistrationRequest request) {
+        customerService.addCustomer(request);
+    }
 }
